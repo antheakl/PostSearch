245da18b066e915515558f0779a80522049ec396
Anthea <kleineranthea@gmail.com>
2023-10-02 22:30:52 +0200

    MorePrettyVersion

--- WebProgrammierungAufgabe2 Neuer/SearchPostID.js
+++ WebProgrammierungAufgabe2 Neuer/SearchPostID.js
@@ -1,34 +1,65 @@
 // Funktion zum Suchen von Posts
 async function searchPosts() {
     try {
-        //Modal für Suchergebnisse erzeugen und vorherige Ergebnisse löschen
+        // Modal für Suchergebnisse erzeugen und vorherige Ergebnisse löschen
         const resultsDiv = document.getElementById("results");
         resultsDiv.innerHTML = "";
         const searchTerm = document.getElementById("search-input").value;
-        //Suche in API eingeben
-        const response = await fetch(`https://dummyjson.com/posts/search?q=${searchTerm}`);
-        const data = await response.json();
-        //Wenn Suchbegriff eingegeben wurde, der in keinem Post vorkommt
-        if (!data.posts || data.posts.length === 0) {
-            resultsDiv.innerHTML = "<p>Keine Posts mit diesem Inhalt gefunden.</p>";
-        }
-        //Erstellen von Links zur Detailansicht mit dem Titel des Posts
-        for (let post of data.posts || []) {
-            const ul = document.createElement("ul");
-            const li = document.createElement("li");
-            const link = document.createElement("a");
-            link.textContent = post.title;
-            link.href = `Detailview.html?id=${post.id}`;
-            li.appendChild(link);
-            ul.appendChild(li);
-            resultsDiv.appendChild(ul);
+        if (searchTerm.trim() === "") {
+            // Wenn die Suchanfrage leer ist, zeige eine Nachricht an
+            resultsDiv.innerHTML = "<p>Geben Sie bitte einen Suchbegriff ein.</p>";
+        } else {
+            // Suche in API eingeben
+            const response = await fetch(`https://dummyjson.com/posts/search?q=${searchTerm}`);
+            const data = await response.json();
+            // Wenn Suchbegriff eingegeben wurde, der in keinem Post vorkommt
+            if (!data.posts || data.posts.length === 0) {
+                resultsDiv.innerHTML = "<p>Keine Posts mit diesem Inhalt gefunden.</p>";
+            } else {
+                for (let post of data.posts || []) {
+                    // Erstellen einer Bootstrap-Card für jeden Post
+                    const cardDiv = document.createElement("div");
+                    cardDiv.className = "card";
+                    cardDiv.style = "width: 18rem;";
+                    const cardBodyDiv = document.createElement("div");
+                    cardBodyDiv.className = "card-body";
+
+                    // Card-Titel
+                    const cardTitle = document.createElement("h5");
+                    cardTitle.className = "card-title";
+                    cardTitle.textContent = post.title;
+
+                    // Weitere Informationen (Card-Subtitle oder Card-Text)
+                    const cardSubtitle = document.createElement("h6");
+                    cardSubtitle.className = "card-subtitle mb-2 text-body-secondary";
+                    cardSubtitle.textContent = `${post.reactions} Reaktionen`;
+
+                    const cardText = document.createElement("p");
+                    cardText.className = "card-text";
+                    cardText.textContent = `Tags: ${post.tags}`;
+
+                    // Link zur Detailansicht
+                    const cardLink = document.createElement("a");
+                    cardLink.className = "card-link";
+                    cardLink.textContent = "Details";
+                    cardLink.href = `Detailview.html?id=${post.id}`;
+
+                    cardBodyDiv.appendChild(cardTitle);
+                    cardBodyDiv.appendChild(cardSubtitle);
+                    cardBodyDiv.appendChild(cardText);
+                    cardBodyDiv.appendChild(cardLink);
+                    cardDiv.appendChild(cardBodyDiv);
+                    resultsDiv.appendChild(cardDiv);
+                }
+            }
         }
     } catch (error) {
-        //Fehleranzeige in Konsole und auf "normalem" Bildschirm
+        // Fehleranzeige in Konsole und auf "normalem" Bildschirm
         console.error("Da ist etwas bei der Suche falsch gelaufen..sorry!", error);
         displayError("Da ist etwas bei der Suche falsch gelaufen..sorry!");
     }
 }
+
 
 // Event Listener hinzufügen, um die Postdetails aufzurufen, wenn auf einen Posttitel geklickt wird.
 document.addEventListener("DOMContentLoaded", function () {
